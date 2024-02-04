ab757c8c634708ea6bedb236bb90bdf7ca4026fa
Muhammad Saqib Nawaz <muhammadsaqib8379@gmail.com>
2024-02-04 15:07:29 +0500

    Completed Header

---
+++ saqib-portfolio/src/app/components/header/Header.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <a href="/">
+          <Image className={styles.img} src={"/img/saqib-img-4.png"} alt="Saqib Image" width={50} height={50} />
+        </a>
+        <ul className={styles.ul}>
+          <li className={styles.li}>
+            <a href="/">Home</a>
+          </li>
+          <li className={styles.li}>
+            <button>Projects</button>
+          </li>
+          <li className={styles.li}>
+            <button>Contact</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
