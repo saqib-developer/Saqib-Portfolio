527adea5bdb87c9f2ad3bec44c064f86a30d9292
Muhammad Saqib Nawaz <muhammadsaqib8379@gmail.com>
2024-04-16 21:08:20 +0500

    fixed meta tag

--- saqib-portfolio/src/app/layout.tsx
+++ saqib-portfolio/src/app/layout.tsx
@@ -38,8 +38,8 @@ export default function RootLayout({
         <meta property="og:type" content="website" />
         <meta property="og:image:type" content="image/png" />
         {/* <!-- Any size up to 300. Anything above 300px will not work in WhatsApp --> */}
-        <meta property="og:image:width" content="170" />
-        <meta property="og:image:height" content="199" />
+        <meta property="og:image:width" content="258" />
+        <meta property="og:image:height" content="260" />
         {/* <!-- Website to visit when clicked in Facebook or WhatsApp--> */}
         <meta property="og:url" content="https://saqibdev.netlify.app/" />
       </head>
