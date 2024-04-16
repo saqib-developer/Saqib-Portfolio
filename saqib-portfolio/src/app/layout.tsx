ca29f0490bd901de1012cffa84527fb819f7375d
Muhammad Saqib Nawaz <muhammadsaqib8379@gmail.com>
2024-04-16 20:27:27 +0500

    setting meta tags

--- saqib-portfolio/src/app/layout.tsx
+++ saqib-portfolio/src/app/layout.tsx
@@ -22,14 +22,25 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="/Favicon/favicon-16x16.png" />
         <link rel="manifest" href="/Favicon/site.webmanifest" />
         <meta name="google-site-verification" content="PRC5k_u6e51a9it1DW_BNyphZL4K8QoDgF24xzFBxjE" />
-        <meta property="og:title" content="Saqib's Portfolio" />
+
+        {/* <!-- for Microsoft apps--> */}
+        <meta name="msapplication-TileImage" content="/avatar.png" />
+        {/* <!-- for Facebook & Whatsapp -> */}
+        {/* <!-- Site Name, Title, and Description to be displayed --> */}
+        <meta property="og:site_name" content="Saqib's Portfolio" />
+        <meta property="og:title" content="Saqib Projects and Experience Preview" />
         <meta
           property="og:description"
           content="Welcome to Saqib's portfolio, where creativity meets functionality. Dive into a collection of innovative web projects."
         />
-        <meta property="og:image" itemProp="image" content="/avatar.png" />
+        {/* <!-- Image to display --> */}
+        <meta property="og:image" content="/path/to/your/image.png" />
         <meta property="og:type" content="website" />
         <meta property="og:image:type" content="image/png" />
+        {/* <!-- Any size up to 300. Anything above 300px will not work in WhatsApp --> */}
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+        {/* <!-- Website to visit when clicked in Facebook or WhatsApp--> */}
         <meta property="og:url" content="https://saqibdev.netlify.app/" />
       </head>
       <body className={inter.className}>{children}</body>
